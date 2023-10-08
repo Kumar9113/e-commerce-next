19015d81305e483c0164a102fe6f3368801fb2e9
Kumar9113 <12006015@svuceedu.onmicrosoft.com>
2023-10-08 18:21:20 +0530

    intial

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
+
+import Navbar from '@/components/Navbar'
+
 import './globals.css'
 import { Inter } from 'next/font/google'
+import GlobalState from '@/context'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +15,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <GlobalState>
+          <Navbar></Navbar>
+          <main className='flex min-h-screen flex-col mt-[80px]'>{children}</main>
+        </GlobalState>
+      </body>
     </html>
   )
 }
