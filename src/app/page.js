f05e41ce596ec017887183cd5aa4bbe31c5a42a1
Kumar9113 <12006015@svuceedu.onmicrosoft.com>
2023-11-08 12:56:12 +0530

    ruppes-at-home

--- src/app/page.js
+++ src/app/page.js
@@ -111,7 +111,7 @@ export default function Home() {
                             {productItem.name}
                           </h3>
                           <p className="mt-1 text-sm text-gray-800">
-                            ${productItem.price}{" "}
+                            ₹{productItem.price}{" "}
                             <span className="text-red-700">{`(-${productItem.priceDrop}%) Off`}</span>
                           </p>
                         </div>
